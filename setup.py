506f2382f551281f93a9a4f0d14d105a6e900bdc
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-06-26 08:51:56 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='macrobase',
-    version='0.0.9',
+    version='0.0.10',
     packages=find_packages(),
     url='https://github.com/mbcores/macrobase',
     license='MIT',
