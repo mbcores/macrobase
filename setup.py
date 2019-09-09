ec1cb3ed6c980976aa62402ce341451058266037
Alex Sandakov <sandakov.aleksey65@gmail.com>
2019-09-09 14:36:15 +0500

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='macrobase',
-    version='0.0.11',
+    version='0.0.13',
     packages=find_packages(),
     url='https://github.com/mbcores/macrobase',
     license='MIT',
