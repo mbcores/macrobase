c8864984d67eeba628dcb587b6ae41d7c95d872b
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-10-17 17:04:04 +0500

    add CLI to application make synchronous application

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='macrobase',
-    version='0.0.13',
+    version='1.0.0',
     packages=find_packages(),
     url='https://github.com/mbcores/macrobase',
     license='MIT',
