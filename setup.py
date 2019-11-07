f2de6632e1fdf7876d044a02c6707237fc767fb8
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2019-11-07 16:06:56 +0500

    use last drivers

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='macrobase',
-    version='2.0.1',
+    version='2.1.0',
     packages=find_packages(),
     url='https://github.com/mbcores/macrobase',
     license='MIT',
@@ -10,7 +10,7 @@ setup(
     author_email='alexey.shagaleev@yandex.ru',
     description='Macrobase framework for build mAcroservices',
     install_requires=[
-        'macrobase-driver>=1.0.0,<2.0.0',
+        'macrobase-driver>=2.0.0,<3.0.0',
         'structlog==19.2.0'
     ]
 )
