5d93b95af1ff078434805e32f78b0ffd63912c97
Shagaleev Alexey <alexey.shagaleev@yandex.ru>
2020-06-26 23:11:12 +0500

    add flake8

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 from setuptools import setup, find_packages
 
+requirements = [
+    'macrobase-driver>=2.0.0,<3.0.0',
+    'structlog==19.2.0',
+]
+
+test_requirements = [
+    'pytest==5.4.3',
+    'pytest-cov==2.10.0',
+]
+
+dev_requirements = test_requirements + [
+    'flake8',
+    'flake8-import-order'
+]
+
+
 setup(
     name='macrobase',
     version='2.1.0',
@@ -9,8 +25,9 @@ setup(
     author='Alexey Shagaleev',
     author_email='alexey.shagaleev@yandex.ru',
     description='Macrobase framework for build mAcroservices',
-    install_requires=[
-        'macrobase-driver>=2.0.0,<3.0.0',
-        'structlog==19.2.0'
-    ]
+    install_requires=requirements,
+    extras_require={
+        'dev': dev_requirements,
+        'test': test_requirements,
+    }
 )
